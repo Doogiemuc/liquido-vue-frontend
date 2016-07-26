729286d2840f6f19ddd8f38e5d54e3568c49cf04
Robert <github@doogie.de>
2016-07-26 07:58:45 +0000

    Working on nightwatch.js E2E tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -9,13 +9,15 @@ module.exports = {
       //check that idas have been loaded
       .waitForElementVisible('table.doogie-table > tbody > tr:nth-child(2)', 5000)
       .assert.containsText('table.doogie-table > tbody > tr:nth-child(2)', 'Idea 02')
+      .pause(1000)
       
       //test table filter
-      .setValue('input[type=text][name="query"]', 'Idea 10')
+      .setValue('input[name=query]', 'Idea 10')
       .elements('css selector', 'table.doogie-table > tbody > tr', function(result) {
         this.assert.equal(result.value.length, 1, 'Filterd table has 1 row')
       })
-      .assert.containsText('table.doogie-table > tbody > tr:nth-child(2)', 'Idea 10')
+      .assert.containsText('table.doogie-table > tbody > tr', 'Idea 10')
+      .pause(1000)
       
       .end()
   }
